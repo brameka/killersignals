7f73a70f08cefefda9a68ce9504491ea6b0a6eb4
Bronson Rameka <bronson.rameka@perthmint.com.au>
2016-08-15 12:48:43 +0800

    added signals api

--- routes/signal_routes.js
+++ routes/signal_routes.js
@@ -7,9 +7,27 @@ module.exports = function(app, express, router){
       res.json({ message: 'signals api' });
     });
 
+    router.post('/signals', function(req, res) {
+      //console.log(req.body.arr[0]); //array
+      var signal = req.body.signal;
+      var currency = req.body.currency;
+      var description = req.body.description;
+      var stoploss = req.body.stoploss;
+      var takeprofit = req.body.takeprofit;
+
+      res.json({ 
+            signal: signal,
+            currency: currency,
+            description: description,
+            stoploss: stoploss,
+            takeprofit: takeprofit
+        });
+    });
+
     // route to return all users (GET http://localhost:8080/api/users)
     router.get('/beats', function(req, res) {
         res.json({ message: 'beats api' });
+        //console.log(req.body.arr[0]);
         /*var user_id = req.query.user_id;
         console.log("userid: " + user_id);
         var id = mongoose.Types.ObjectId(user_id);
